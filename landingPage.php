f6d4c00fd6226f7db7cd09efab18e45cfaff34ad
flewpher <danielalonsomoreno5606@gmail.com>
2024-05-06 15:39:06 +0200

    modified:   index.php 	new file:   landingPage.php

---
+++ landingPage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amarily Moreno</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+</head>
+
+<body>
+    <section class="animated-background">
+        <div id="stars1"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+    </section>
+
+    <div class="profile-picture">
+        <img src="photo1710950573.jpeg" alt="Foto de perfil">
+
+    </div>
+    <h2>Amarily Moreno</h2>
+
+    <div id="links">
+        <a class="link" href="https://www.facebook.com/profile.php?id=100083344628765" target="_blank">
+            <i class="fab fa-facebook">&nbsp;</i>Facebook
+        </a>
+        <a class="link" href="https://www.instagram.com/masaje_egipcio_murcia?igsh=MWxqY2g0MXhkenUzYQ==" target="_blank">
+            <i class="fab fa-instagram">&nbsp;</i>Instagram
+        </a>
+        <a class="link" href="https://www.tiktok.com/@masajesconama?_t=8bdUn9cmYEk&_r=1&fbclid=PAAabjo3JBsXsbKGCJpwxWEqD2gVMpvawLUrrS8ro8KOGwxVzf9Cw-vbq-xD8" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="0.88em" height="1em" viewBox="0 0 448 512"><path fill="currentColor" d="M448 209.91a210.06 210.06 0 0 1-122.77-39.25v178.72A162.55 162.55 0 1 1 185 188.31v89.89a74.62 74.62 0 1 0 52.23 71.18V0h88a121.18 121.18 0 0 0 1.86 22.17A122.18 122.18 0 0 0 381 102.39a121.43 121.43 0 0 0 67 20.14Z"/></svg> TikTok
+        </a>
+        <a class="link" href="https://www.youtube.com/@masaje_egipcio_murcia" target="_blank">
+            <i class="fab fa-youtube">&nbsp;</i>Youtube
+        </a>
+        <a class="whatsapp-link" href="https://wa.me/34640758231?text=Hola%20me%20gustaría%20contactarte" target="_blank">
+            <i class="fab fa-whatsapp"></i>
+        </a>
+    </div>
+
+</body>
+
+</html>
